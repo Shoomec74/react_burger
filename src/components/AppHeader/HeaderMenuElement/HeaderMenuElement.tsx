243310d7002a396ffe24abb98ab57525c2743c89
Evgeny Rusakov <shoomec@yandex.ru>
2023-08-10 23:06:17 +0500

    load backup project

--- src/components/AppHeader/HeaderMenuElement/HeaderMenuElement.tsx
+++ src/components/AppHeader/HeaderMenuElement/HeaderMenuElement.tsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { FC } from "react";
 import headerMenuElementStyles from "./headerMenuElement.module.css";
-import PropTypes from "prop-types";
+import { NavLink } from "react-router-dom";
 
-const HeaderMenuElement = (props) => {
-  const { element, title, link } = headerMenuElementStyles;
+interface IProps {
+  text: string;
+  children: React.ReactNode;
+  path: string;
+}
+
+const HeaderMenuElement: FC<IProps> = (props) => {
+  const { element, title, link, activeLink } = headerMenuElementStyles;
   return (
     <li className={`${element} pl-5 pr-5`}>
-      <a className={link} href="#">
+      <NavLink className={link} to={props.path} activeClassName={activeLink}>
         <p
           className={`${title} ${
             props.text === "default"
@@ -16,14 +22,9 @@ const HeaderMenuElement = (props) => {
         >
           {props.children}
         </p>
-      </a>
+      </NavLink>
     </li>
   );
 };
-
-HeaderMenuElement.propTypes ={
-  text: PropTypes.string.isRequired,
-  children: PropTypes.node.isRequired
-}
 
 export default HeaderMenuElement;
